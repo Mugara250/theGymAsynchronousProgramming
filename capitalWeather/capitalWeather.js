7ce6c50507aa8b411c047c1a53e9403447df715b
Mugara250 <mbonyumugara52@gmail.com>
2025-06-05 18:26:36 +0200

    fetch the temperature of the capital city of a country

---
+++ capitalWeather/capitalWeather.js
@@ -0,0 +1,67 @@
+async function capitalWeather(countryName) {
+    const url1 = `https://restcountries.com/v3.1/name/${countryName}`;
+
+    try {
+        // fetching data about the country
+        let response1 = await fetch(url1);
+        if (!response1.ok) throw new Error("Fetch failed!");
+        let countryObj = await response1.json();
+        let capital = countryObj[0].capital[0];
+        let lat = countryObj[0].capitalInfo.latlng[0];
+        let lon = countryObj[0].capitalInfo.latlng[1];
+
+        // fetching data about the weather of the capital city
+        const url2 = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current_weather=true`;
+        let response2 = await fetch(url2);
+        if (!response2.ok) throw new Error("Fetch failed!");
+        let weatherObj = await response2.json();
+        let currentTemperature = weatherObj.current_weather.temperature;
+
+        return {
+            countryName,
+            capital,
+            currentTemperature
+        }
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+capitalWeather('Rwanda')
+    .then(result => {
+        console.log(result);
+    })
+    .catch(err => {
+        console.log(err);
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
